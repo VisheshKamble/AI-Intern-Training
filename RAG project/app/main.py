3cbc1ff621b1c54ed106771a17d4b95ffd4f7670
Vishesh Kamble <visheshy2k17@gmail.com>
2026-06-03 16:35:06 +0530

    Text Splitters implementation in RAG Project

--- RAG project/app/main.py
+++ RAG project/app/main.py
@@ -2,6 +2,7 @@ from dotenv import load_dotenv
 from langchain_groq import ChatGroq #Groq
 from langchain_community.document_loaders import PyPDFLoader, TextLoader
 from langchain_core.prompts import ChatPromptTemplate
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 load_dotenv()
 
@@ -9,9 +10,16 @@ load_dotenv()
 
 #docs = data.load()
 
-data = PyPDFLoader("RAG project/GRU.pdf") #Loading the PDF file 
+data = PyPDFLoader("RAG project/deeplearning.pdf") #Loading the PDF file 
 
 docs = data.load()
+
+splitter = RecursiveCharacterTextSplitter(
+    chunk_size = 1000, 
+    chunk_overlap = 200
+)
+
+chunks = splitter.split_documents(docs)
 
 template = ChatPromptTemplate.from_messages([
     ('system',"""You are a helpful assistant for question answering. """),
