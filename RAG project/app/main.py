e153f0564fb20c0e49471b3abad722293bc76de1
Vishesh Kamble <visheshy2k17@gmail.com>
2026-06-04 13:09:06 +0530

    RAG application logic done UI remaining

--- RAG project/app/main.py
+++ RAG project/app/main.py
@@ -1,35 +1,60 @@
 from dotenv import load_dotenv
 from langchain_groq import ChatGroq #Groq
-from langchain_community.document_loaders import PyPDFLoader, TextLoader
+from langchain_community.vectorstores import Chroma
+from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_core.prompts import ChatPromptTemplate
-from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 load_dotenv()
 
-#data = TextLoader("RAG project/notes.txt") #loading the text file
+model = ChatGroq(model="meta-llama/llama-4-scout-17b-16e-instruct")
 
-#docs = data.load()
+embedding_models = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
 
-data = PyPDFLoader("RAG project/deeplearning.pdf") #Loading the PDF file 
-
-docs = data.load()
-
-splitter = RecursiveCharacterTextSplitter(
-    chunk_size = 1000, 
-    chunk_overlap = 200
+vectorstore = Chroma(
+    persist_directory="chroma-db",
+    embedding_function=embedding_models
 )
 
-chunks = splitter.split_documents(docs)
+retriever = vectorstore.as_retriever(
+    search_type = "mmr",
+    search_kwargs = {"k": 4, "fetch_k": 10 , "lambda_mult": 0.5}
+)
 
-template = ChatPromptTemplate.from_messages([
-    ('system',"""You are a helpful assistant for question answering. """),
-    ('user', "{data}")
-])
+llm = ChatGroq( model = "meta-llama/llama-4-scout-17b-16e-instruct")
 
-model = ChatGroq(model = 'meta-llama/llama-4-scout-17b-16e-instruct')
+#prompt template 
 
-prompt = template.invoke({"data": docs[0].page_content})
+prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system",
+         """You are a helpful AI assistant.
+         
+         Use Only the provided context to answer the question.
+         
+         If the answer is not present in the context ,
+         say that answer is not available in the document."""),
+        ("human", """Context: {context}
+         
+         Question: {question}""")
+    ]
+)
 
-response = model.invoke(prompt)
+print ("Welcome to RAG System! Enter 0 to exit")
 
-print(response.content)
+while True:
+    query = input("You : ")
+    if query == "0":
+        break
+    docs = retriever.invoke(query)
+    context = "\n\n".join([doc.page_content for doc in docs])
+
+    final_prompt = prompt.invoke(
+        {
+            "context": context,
+            "question": query
+        }
+    )
+
+    response = llm.invoke(final_prompt)
+
+    print("Bot : ", response.content)
