aef9e389188fa8644587766452e15ad05040fc03
Vishesh Kamble <visheshy2k17@gmail.com>
2026-06-04 16:00:09 +0530

    parallel runnables

---
+++ GenAI-pt3/parallelrunnables.py
@@ -0,0 +1,33 @@
+from dotenv import load_dotenv
+load_dotenv()  
+
+from langchain_groq import ChatGroq
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnableParallel
+
+model = ChatGroq(model="meta-llama/llama-4-scout-17b-16e-instruct")
+
+parser = StrOutputParser()
+
+#two different prompts
+
+prompt1 = ChatPromptTemplate.from_template(
+    "Explain the {topic} in short"
+    )
+
+prompt2 = ChatPromptTemplate.from_template(
+    "What are the applications of {topic} ?"
+)
+
+topic = "Machine Learning"
+
+chain = RunnableParallel ({
+    "explain" :prompt1 | model | parser,
+    "applications" : prompt2 | model | parser
+})
+
+result = chain.invoke({ "topic" : topic })
+
+print(result['explain'])
+print(result['applications'])
