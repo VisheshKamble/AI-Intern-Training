0554e2ce2b9878248261edcade0b39985fcf1978
Vishesh Kamble <visheshy2k17@gmail.com>
2026-06-08 14:54:35 +0530

    AI Agent 2 in LangGraph

--- LangGraph/ai_agent2.py
+++ LangGraph/ai_agent2.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 from langchain_groq import ChatGroq
 from langchain_core.messages import HumanMessage, AIMessage
 from langgraph.graph import StateGraph, START, END
-from typing import TypedDict, List , Dict , Union
+from typing import TypedDict, List , Union
 
 load_dotenv()
 
@@ -13,3 +13,37 @@ class AgentState(TypedDict):
 
 llm = ChatGroq(model="meta-llama/llama-4-scout-17b-16e-instruct")
 
+def process(state: AgentState) -> AgentState:
+    """This node will solve the request you input"""
+
+    response = llm.invoke(state["messages"])
+    state["messages"].append(AIMessage(content=response.content))
+    print(f"AI: {response.content}")
+    return state
+
+graph = StateGraph(AgentState)
+graph.add_node("process", process)
+graph.add_edge(START, "process")
+graph.add_edge("process", END) 
+agent = graph.compile()
+
+conversation_history = []
+
+user_input = input("You: ")
+while user_input.lower() != "exit":
+    conversation_history.append(HumanMessage(content=user_input))
+    result = agent.invoke({"messages": conversation_history})
+    conversation_history = result["messages"]
+    user_input = input("Enter: ")
+
+with open("logging.txt", "w") as file:
+    file.write("Your Conversation Log:\n")
+    
+    for message in conversation_history:
+        if isinstance(message, HumanMessage):
+            file.write(f"You: {message.content}\n")
+        elif isinstance(message, AIMessage):
+            file.write(f"AI: {message.content}\n\n")
+    file.write("End of Conversation")
+
+print("Conversation saved to logging.txt")
